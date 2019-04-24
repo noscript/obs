bbef7930fad2a4a727cd4b2c2c37542496b3ddf7
Sergey Vlasov <sergey@vlasov.me>
2019-04-24 17:22:05 +0300

    added template

---
+++ template/TEMPLATE_NAME.dsc
@@ -0,0 +1,7 @@
+Format: 3.0 (quilt)
+Source: TEMPLATE_NAME
+Binary: TEMPLATE_NAME
+Version: _DUMMY_
+DEBTRANSFORM-RELEASE: 1
+Standards-Version: 4.1.1
+Build-Depends: debhelper (>= 10)
---
+++ template/config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $(basename $0) <name>" >&2
+  exit 1
+fi
+
+NEW_NAME=$1
+
+sed -i "s/TEMPLATE_NAME/$NEW_NAME/g" debian.* _service *.dsc
+mv TEMPLATE_NAME.dsc $NEW_NAME.dsc
+
+echo "Update git url:"
+grep "https://github.com/$NEW_NAME/$NEW_NAME" *
