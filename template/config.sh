bd3b59d91ca5eccb3710b9f2564396b9d0296535
Sergey Vlasov <sergey@vlasov.me>
2019-05-23 11:24:06 +0300

    template update

--- template/config.sh
+++ template/config.sh
@@ -1,16 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-if [ $# -ne 1 ]; then
+if [[ $# -ne 1 ]]; then
   echo "Usage: $(basename $0) <name>" >&2
   exit 1
 fi
 
 NEW_NAME=$1
 
+cd $(dirname $0)
+mkdir -p ../$NEW_NAME
+cp $(ls -I config.sh) ../$NEW_NAME
+cd ../$NEW_NAME
+
 sed -i "s/TEMPLATE_NAME/$NEW_NAME/g" debian.* _service *.dsc
-git mv TEMPLATE_NAME.dsc $NEW_NAME.dsc
+mv TEMPLATE_NAME.dsc $NEW_NAME.dsc
 
 echo "Update git url:"
 grep "https://github.com/$NEW_NAME/$NEW_NAME" *
-
-rm -f config.sh
