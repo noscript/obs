6567265b6dde2df7ba8b2907ea5818f0c4ed5987
Sergey Vlasov <sergey@vlasov.me>
2019-06-10 17:24:15 +0300

    template -> _template

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 if [[ $# -ne 1 ]]; then
-  echo "Usage: $(basename $0) <path to home project>"
+  echo "Usage: $(basename $0) <path to home project>" >&2
   exit 1
 fi
 
 HOME_PROJECT=$1
 
 cd $(dirname $0)
-find . -name '_service' -not -path "./template/*" | xargs -I{} bash -x -c 'cp {} "'$HOME_PROJECT'/{}"'
+find . -name '_service' -not -path "./_template/*" | xargs -I{} bash -x -c 'cp {} "'$HOME_PROJECT'/{}"'
